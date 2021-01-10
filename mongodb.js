9803c3bbe540117de4a1996709b7edd3c66b54d1
Surabhi2481 <surabhibbsr2481@gmail.com>
2021-01-10 21:51:52 +0530

    generating ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,18 @@
 //CRUD create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
+//console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     if(error){
@@ -16,8 +24,9 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     //console.log("Connected Correctly!")
     
     // db.collection('users').insertOne({
-    //     name : 'Surabhi',
-    //     age : 22
+        
+    //     name : 'Jyoti',
+    //     age : 23
     // }, (error,result) => {
     //     if(error)
     //     {
@@ -44,26 +53,24 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     //     console.log(result.ops)
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description : 'Water the plants',
-            completed : true
-        },
-        {
-            description : 'Fill the bottles',
-            completed : true
-        },
-        {
-            description : 'Clean the living room',
-            completed : false
-        }
-    ], (error, result) => {
-        if(error){
-            return console.log("Unable to insert the documents")
-        }
+//     db.collection('tasks').insertMany([
+//         {
+//             description : 'Water the plants',
+//             completed : true
+//         },
+//         {
+//             description : 'Fill the bottles',
+//             completed : true
+//         },
+//         {
+//             description : 'Clean the living room',
+//             completed : false
+//         }
+//     ], (error, result) => {
+//         if(error){
+//             return console.log("Unable to insert the documents")
+//         }
 
-        console.log(result.ops)
-    })
-
-    
-})
+//         console.log(result.ops)
+//     })    
+ })
