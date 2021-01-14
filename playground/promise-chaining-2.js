7885e9b2a38ab0eacd24f69b302a34e1253c302a
Surabhi2481 <surabhibbsr2481@gmail.com>
2021-01-14 18:37:39 +0530

    implemented async/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -9,3 +9,15 @@ Task.findByIdAndDelete('5ffd5e3068405a2d18ef0d79').then((task) => {
 }).catch((e) => {
     console.log(e)
 })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed : false })
+    return count
+} 
+
+deleteTaskAndCount('5ffed318e6305b56e801c73b').then((count) => {
+    console.log(count)
+}).catch((e) => {
+    console.log(e)
+}) 
