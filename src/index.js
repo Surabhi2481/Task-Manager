84447456269a055493f3bcf44dd6fb7477c7de17
Surabhi2481 <surabhibbsr2481@gmail.com>
2021-01-15 19:04:15 +0530

    resource deleting endpoints

--- src/index.js
+++ src/index.js
@@ -76,6 +76,19 @@ app.get('/users/:id', async (req,res) => {
     console.log(req.params)
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 app.post('/tasks', async (req,res) => {
     const task = new Task(req.body)
 
@@ -90,6 +103,14 @@ app.post('/tasks', async (req,res) => {
 })
 
 app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation){
+        return res.status(400).send({error : 'Invalid Updates!' })
+    }
+
     try{
         const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new : true, runValidators : true})
         
@@ -131,16 +152,19 @@ app.get('/tasks/:id', async (req, res) => {
     catch (e){
         res.status(500).send()
     }
-
-    // Task.findById({_id}).then((task) => {
-    //     if(!task){
-    //         return res.status(404).send()
-    //     }
-    //     res.send(task)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
     console.log(req.params)
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try{   
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e){
+        res.status(500).send(e)
+    }
 })
 
 app.listen(port, () => {
