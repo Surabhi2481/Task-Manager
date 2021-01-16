59c4856b5e3e51fe0e18e045c1347005537c3ac3
Surabhi2481 <surabhibbsr2481@gmail.com>
2021-01-16 23:50:25 +0530

    generation authentication tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,7 +7,8 @@ router.post('/users', async (req, res) => {
 
     try{
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token})
     }
     catch (e) {
         res.status(400).send(e)
@@ -42,7 +43,11 @@ router.patch('/users/:id', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try{
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        if(!token){
+            console.log('Token is empty')
+        }
+        res.send({ user, token})
     } catch (e) {
         res.status(400).send(e)
     }
