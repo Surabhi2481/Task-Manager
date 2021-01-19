83cd2723e2988e20845c43ead51a62031754c4be
Surabhi2481 <surabhibbsr2481@gmail.com>
2021-01-19 20:02:07 +0530

    filtering data

--- src/routers/task.js
+++ src/routers/task.js
@@ -50,10 +50,17 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 })
 
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
 
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
     try {
         //const task = await Task.find({owner : req.user._id})
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
         res.send(req.user.tasks)
     }
     catch (e) {
@@ -64,7 +71,6 @@ router.get('/tasks', auth, async (req, res) => {
 
 router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
-
     try {
         //const task = await Task.findById({_id})
 
@@ -82,7 +88,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
     try{   
-        const task = await Task.findOne({_id : req.params.id, owner : req.user._id})
+        const task = await Task.findOneAndDelete({_id : req.params.id, owner : req.user._id})
 
         if(!task){
             return res.status(404).send()
